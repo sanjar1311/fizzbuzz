f4da1334b47b4e39cce6eabe097f158767b9007e
sanjar1311 <sanjarabduxamidov1311@gmail.com>
2020-12-09 22:25:13 +0500

    add js

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var elForm = document.querySelector('.js-form');
+var elInput = document.querySelector('.js-input');
+var elBtn = document.querySelector('.js-btn');
+var elOutput = document.querySelector('.js-output');
+
+var fizz = 'Fizz';
+var buzz = 'Buzz';
+var fizzbuzz = 'FizzBuzz';
+
+
+elForm.addEventListener('click', function(evt) {
+  evt.preventDefault();
+
+  if(elInput.value % 3 === 0) {
+    elOutput.textContent = fizz;
+  }
+
+});
